b267bdda29e0c8c14cfb8b0e7d2ba21fd4ad8d5a
Nguyen Minh Huy Duong <minhhuy1411417@gmail.com>
2021-01-21 02:39:22 -0800

    Add tree structure: Insert interation and recursive. search interation and recursive. Need delete, ALV Print in order, pre order

--- data_structure/Tree/search_tree.py
+++ data_structure/Tree/search_tree.py
@@ -11,19 +11,16 @@ class node:
         del self
 
 class search_tree:
-    def __init__(self):
-        self.root = None
-        self.size = 0
-
-    def __init__(self, data):
+    def __init__(self, data=None):
         self.root = node(data)
-        self.size = 1
+        self.size = 0
 
     def insert(self, data):
         if self.root is None:
             self.root = node(data)
+            self.size += 1
         else:
-            self.insert(self.root, data)
+            self.insert_recursively(self.root, data)
 
     def insert_recursively(self, root, data):
         if root.data == data:
@@ -31,14 +28,74 @@ class search_tree:
         elif root.data < data:
             if root.right is None:
                 root.right = node(data)
+                self.size += 1
             else:
                 self.insert_recursively(root.right, data)
         else:
             if root.left is None:
                 root.left = node(data)
+                self.size += 1
             else:
                 self.insert_recursively(root.left, data)
 
+    def insert_interation(self, data):
+        if self.root is None:
+            self.root = node(data)
+            self.size += 1
+        else:
+            current = self.root
+            input = node(data)
+            while True:
+                if current.data == data:
+                    current.data = data
+                    self.size -= 1
+                    break
+                elif current.data > data:
+                    if current.left is None:
+                        current.left = input
+                        break
+                    current = current.left
+                else:
+                    if current.right is None:
+                        current.right = input
+                        break
+                    current = current.right
+            self.size += 1
+
+    def search(self, key):
+        if self.root is None:
+            return False
+        else:
+            return self.search_recursively(key, self.root)
+
+    def search_recursively(self, key, root):
+        if root.data == key:
+            return root
+        elif root is None:
+            return False
+        elif root.data > key:
+            return self.search_recursively(key, root.left)
+        else:
+            return self.search_recursively(key, root.right)
+
+    def search_interation(self, key):
+        if self.root is None:
+            return False
+        else:
+            current = self.root
+            while current is not None:
+                if current.data == key:
+                    return current
+                elif current.data > key:
+                    current = current.left
+                else:
+                    current = current.right
+            return False
+
 
 if __name__ == "__main__":
-    pass
+    a = search_tree(20)
+    b=[10,5,31,28,11]
+    for i in b:
+        a.insert(i)
+    print(a.root.left.right)
