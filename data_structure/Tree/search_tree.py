ddc3759407dc2b9c7e6902a012be00270337bd7a
Nguyen Minh Huy Duong <minhhuy1411417@gmail.com>
2021-01-22 02:29:34 -0800

    Add delete function

--- data_structure/Tree/search_tree.py
+++ data_structure/Tree/search_tree.py
@@ -12,8 +12,12 @@ class node:
 
 class search_tree:
     def __init__(self, data=None):
-        self.root = node(data)
-        self.size = 0
+        if data is None:
+            self.root = None
+            self.size = 0
+        else:
+            self.root = node(data)
+            self.size = 1
 
     def insert(self, data):
         if self.root is None:
@@ -92,10 +96,44 @@ class search_tree:
                     current = current.right
             return False
 
+    def delete(self, key):
+        if self.search_interation(key) is False:
+            raise Exception('The data does not exist.')
+        else:
+            self.root = self.delete_recursively(key, self.root)
+
+    def delete_recursively(self, key, root):
+        if root.data == key:
+            if root.left is None and root.right is None:
+                root = None
+                return root
+            elif root.left is None and root.right is not None:
+                temp = root.right
+                root = None
+                return temp
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+            else:
+                min = self.min_node(root.right)
+                root.data = min.data
+                root.right = self.delete_recursively(min.data, root.right)
+        elif root.data > key:
+            root.left = self.delete_recursively(key, root.left)
+        elif root.data < key:
+            root.right = self.delete_recursively(key, root.right)
+        return root
+
+
+    def min_node(self, root):
+        while root.left is not None:
+            root = root.left
+        return root
 
 if __name__ == "__main__":
     a = search_tree(20)
     b=[10,5,31,28,11]
     for i in b:
         a.insert(i)
-    print(a.root.left.right)
+    print(a.search_interation(5).data)
